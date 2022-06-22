92f34ba13ce95aeec691d63d72880aaeba705f8c
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-06-22 10:07:34 -0300

    praticando cSharp no console

---
+++ OnFire/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Globalization;
+
+
+namespace OnFire {
+    class Program {
+        static void Main(string[] args) {
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com a medida do triângulo x: ");
+
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com a medida do triângulo y: ");
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine($"Área de x = {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Área de x = {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            }
+            else {
+                Console.WriteLine("Maior área: Y");
+            }
+
+        }
+    }
+}
